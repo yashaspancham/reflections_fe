d841ec28b17e7fa8a0fda45f7273db572aab3c84
Yashas <yashaspancham@gmail.com>
2025-05-26 15:53:05 +0530

    Yashas-feat-successgifadded

--- app/(tabs)/success.tsx
+++ app/(tabs)/success.tsx
@@ -1,32 +1,30 @@
-import { View,Image } from "react-native";
-import { Text } from "react-native-paper";
+import { View, Image } from "react-native";
 import { useCallback } from "react";
-import { useFocusEffect,router } from "expo-router";
-// import LottieView from "lottie-react-native";
+import { useFocusEffect, router } from "expo-router";
 
 const SuccessPage = () => {
-      useFocusEffect(
-        useCallback(() => {
-    
-          const timeout = setTimeout(() => {
-            router.push("/");
-          }, 700);
-          return () => clearTimeout(timeout);
-        }, [])
-      );
+  useFocusEffect(
+    useCallback(() => {
+      const timeout = setTimeout(() => {
+        router.push("/");
+      }, 7000);
+      return () => clearTimeout(timeout);
+    }, [])
+  );
   return (
     <View
       style={{
         flex: 1,
-        padding: 50,
         alignItems: "center",
         justifyContent: "center",
-        backgroundColor: "#fff", 
+        backgroundColor: "#fff",
       }}
     >
-      <Text variant="displayLarge">Success</Text>
-      <Image source={require("../../assets/images/test.jpg")} style={{width:300,height:300}}/>
-      <Text variant="headlineSmall">Could not figure out Animation yet......have a Puppy</Text>
+      <Image
+        source={require("../../assets/images/successAnimation.gif")}
+        style={{ width: 300, height: 300 }}
+        resizeMode="contain"
+      />
     </View>
   );
 };
