bf34edbf7ab36e1af39b2d644404e4f9d3ca8343
Yashas <yashaspancham@gmail.com>
2025-06-11 17:04:40 +0530

    Yashas-feat-ChangeUserNameDone

--- components/Profile_Info/index.tsx
+++ components/Profile_Info/index.tsx
@@ -1,16 +1,27 @@
 import React, { useEffect } from "react";
 import { View, Image, StyleSheet } from "react-native";
-import { Text, ActivityIndicator, } from "react-native-paper";
+import {
+  Text,
+  ActivityIndicator,
+  IconButton,
+  useTheme,
+  TextInput,
+} from "react-native-paper";
 import { RootState } from "@/utiles/redux/store";
 import { useSelector } from "react-redux";
-import { getUserName,  putUserName, } from "@/utiles/apis/users/users";
+import { getUserName, putUserName } from "@/utiles/apis/users/users";
 import Toast from "react-native-toast-message";
 
 const ProfileInfo = () => {
   const [username, setUsername] = React.useState<string | null>(null);
   const [loadingUsername, setLoadingUsername] = React.useState<boolean>(true);
+  const [editUsername, setEditUsername] = React.useState<boolean>(false);
+  const [disableTextInput, setDisableTextInput] =
+    React.useState<boolean>(false);
+  const [newUsername, setNewUsername] = React.useState<string>("");
   const user_email = useSelector((state: RootState) => state.auth.user_email);
   const user_id = useSelector((state: RootState) => state.auth.user_id);
+  const theme = useTheme();
   useEffect(() => {
     if (user_id !== null && user_id !== undefined) {
       getUserName(user_id).then((res: any) => {
@@ -27,18 +38,24 @@ const ProfileInfo = () => {
       });
     }
   }, []);
- // putUserName(user_id, "kabu").then((res: any) => {
-  //   if (res.success) {
-  //     setUsername(res.data);
-  //   } else {
-  //     Toast.show({
-  //       type: "error",
-  //       text1: "Error",
-  //       text2: res.message || "Failed to update username.",
-  //     });
-  //   }
-  // });
-
+  const changename = () => {
+    setDisableTextInput(true);
+    if (user_id !== null && user_id !== undefined) {
+      putUserName(user_id, newUsername).then((res: any) => {
+        if (res.success) {
+          setUsername(res.data);
+          setEditUsername(false);
+          setDisableTextInput(false);
+        } else {
+          Toast.show({
+            type: "error",
+            text1: "Error",
+            text2: res.message || "Failed to update username.",
+          });
+        }
+      });
+    }
+  };
 
   return (
     <>
@@ -49,8 +66,36 @@ const ProfileInfo = () => {
       <View>
         {loadingUsername ? (
           <ActivityIndicator animating={true} />
+        ) : editUsername ? (
+          <TextInput
+            disabled={disableTextInput}
+            autoFocus={true}
+            multiline={false}
+            numberOfLines={1}
+            dense={true}
+            maxLength={10}
+            value={newUsername}
+            onChangeText={(text) => setNewUsername(text)}
+            style={{ width: 150, textAlign: "center", marginBottom: 15 }}
+            right={
+              <TextInput.Icon
+                icon="check"
+                color="#8fab73"
+                onPress={() => changename()}
+              />
+            }
+          />
         ) : (
-          <Text style={styles.name}>{username}</Text>
+          <View style={styles.usernameContainer}>
+            <Text style={styles.name}>{username}</Text>
+            <IconButton
+              onPress={() => setEditUsername(true)}
+              icon="pen"
+              iconColor={theme.colors.primary}
+              style={{ marginBottom: 10 }}
+              size={20}
+            />
+          </View>
         )}
       </View>
       <Text style={styles.email}>{user_email}</Text>
@@ -60,6 +105,10 @@ const ProfileInfo = () => {
 export default ProfileInfo;
 
 const styles = StyleSheet.create({
+  usernameContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
   avatar: {
     width: 100,
     height: 100,
@@ -71,6 +120,7 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: "bold",
     marginBottom: 4,
+    marginLeft: 40,
   },
   email: {
     fontSize: 16,
